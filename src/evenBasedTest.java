4768c780cd6c0d107e224a97de5f976435216a56
rahan23 <rahanarakkal@gmail.com>
2023-04-04 20:38:06 -0700

    This is a java code

---
+++ src/evenBasedTest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class evenBasedTest {
+
+	public static void main(String[] args) {
+		
+	}
+}
+
+	  class primeExample{
+		  public void testprime() {
+			  int m=0,n;
+			  Scanner scanner=new Scanner(System.in);
+					  System.out.println("Enter number to test prime");
+			  int n=scanner.nextInt();
+			  m=n/2;
+			  if(n==0){
+					  System.out.println("it is a prime number");
+		  }else {
+			  System.out.println("it is not a prime number");
+			  
+			
+		  }
+	  }
+  }
+
+  
+   
+  
+
+
