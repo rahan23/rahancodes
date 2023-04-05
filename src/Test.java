62acf23b5199d9280e976cd4a1362c06a5bdac54
rahan23 <rahanarakkal@gmail.com>
2023-04-04 21:20:05 -0700

    Second code

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+package tele;
+
+class TestCons
+{
+	
+	public TestCons( ) {
+		System.out.println("Hi from Constructor Body");
+	}
+	
+	public TestCons(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+	
+}
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		new TestCons(12, 13);
+		
+	}
+}
---
+++ src/TestException.java
@@ -0,0 +1,23 @@
+package tele;
+
+import java.util.Scanner;
+
+public class TestException {
+	
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter your number");
+		int a,b,c;
+		try
+		{
+	    a=scanner.nextInt();
+	    b=scanner.nextInt();
+	    
+	    c=a/b;
+	    System.out.println(c);
+		}
+		catch (Exception e) {
+			System.out.println("Please do proper input");
+		}
+	}
+}
